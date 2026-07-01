ad3cc5bcd56d72082fec3a8fca3cb309682e9f64
prabhakarsingh2007 <prabhakarkumarsingh578@gmail.com>
2026-07-01 20:56:45 +0530

    fix: serve static files in urls.py for local development and render

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from myapp.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,3 +11,6 @@ urlpatterns = [
     path('contact/', contact, name='contact'),
     path('skills/', skills, name='skills'), 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.BASE_DIR / 'static')
